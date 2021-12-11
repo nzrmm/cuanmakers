3e1dce8e5f44713c3b7c6c5a9c7146be57642216
nzrmm <iamgata.me@gmail.com>
2021-12-12 04:15:48 +0700

    [fix] maintenance and not found handler

---
+++ src/components/templates/TheError.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="min-h-screen flex justify-center items-center">
+    <div class="flex flex-col items-center">
+      <div class="text-8xl">🙁</div>
+      <h2 class="mt-4 mb-10 text-base-secondary font-bold text-6xl uppercase tracking-widest">
+        <slot></slot>
+      </h2>
+      <router-link :to="{name: 'Discover'}" class="px-6 py-3 bg-gradient-to-r from-blue to-purple text-white rounded-12">
+        Back to discover
+      </router-link>
+    </div>
+  </div>
+</template>
--- src/views/Maintenance.vue
+++ src/views/Maintenance.vue
@@ -1,10 +1,3 @@
 <template>
-  <div class="min-h-screen flex justify-center items-center">
-    <div class="flex flex-col items-center">
-      <h2 class="mb-8 text-base-secondary font-bold text-6xl uppercase tracking-widest">Maintenance :(</h2>
-      <router-link :to="{name: 'Discover'}" class="px-6 py-3 bg-gradient-to-r from-blue to-purple text-white rounded-12">
-        Back to discover
-      </router-link>
-    </div>
-  </div>
+      <TheError>Maintenance</TheError>
 </template>
